51e857ce7ebf77a5fbc288259582f1d3f506c863
Jack Newton <eternal.turtles@gmail.com>
2014-07-23 18:20:54 -0500

    year's days array is sum of months' days arrays

--- lib/year.rb
+++ lib/year.rb
@@ -1,5 +1,10 @@
 class Year
 
+  # CELL = "  "
+  MONTH_GUTTER = "  "
+
+  attr_reader :days
+
   def self.leap_year?(year)
     @year = year
     if @year % 4 == 0
@@ -8,6 +13,100 @@ class Year
       end
     end
     false
+  end
+
+  def initialize(year)
+    @year = year
+    self.build_days_array
+  end
+
+  def to_s
+    output = header
+    output << self.build_year
+  end
+
+  def build_year
+    # self.build_days_array
+    self.build_january_february_march_title_line
+    self.build_weeknames_line
+    # self.build_january_february_march_days_array
+    # self.build_weeknames_line
+    # self.build_year_array
+  end
+
+  def build_january_february_march_days_array
+
+  end
+
+  def build_january_february_march_title_line
+    gutter = MONTH_GUTTER.dup
+    title_line = ""
+    title_line << "January".center(20)
+    title_line << gutter
+    title_line << "February".center(20)
+    title_line << gutter
+    title_line << "March".center(20).rstrip
+    title_line << "\n"
+  end
+
+  def build_april_may_june_title_line
+    gutter = MONTH_GUTTER.dup
+    title_line = ""
+    title_line << "April".center(20)
+    title_line << gutter
+    title_line << "May".center(20)
+    title_line << gutter
+    title_line << "June".center(20).rstrip
+    title_line << "\n"
+  end
+
+  def build_july_august_september_line
+    gutter = MONTH_GUTTER.dup
+    title_line = ""
+    title_line << "July".center(20)
+    title_line << gutter
+    title_line << "August".center(20)
+    title_line << gutter
+    title_line << "September".center(20).rstrip
+    title_line << "\n"
+  end
+
+  def build_october_november_december_line
+    gutter = MONTH_GUTTER.dup
+    title_line = ""
+    title_line << "October".center(20)
+    title_line << gutter
+    title_line << "November".center(20)
+    title_line << gutter
+    title_line << "December".center(20).rstrip
+    title_line << "\n"
+  end
+
+  def build_weeknames_line
+    gutter = MONTH_GUTTER.dup
+    weeknames_line = ""
+    weeknames = "Su Mo Tu We Th Fr Sa"
+    weeknames_line << weeknames
+    weeknames_line << gutter
+    weeknames_line << weeknames
+    weeknames_line << gutter
+    weeknames_line << weeknames
+    weeknames_line << "\n"
+  end
+
+  def build_days_array
+    @days = []
+    12.times do |i|
+      i += 1
+      @days << Month.new(i, @year).days
+    end
+    @days
+  end
+
+ private
+
+  def header
+    "#{@year}".rjust(33) + "\n\n" # or rstrip(64)
   end
 
 end
--- spec/year_spec.rb
+++ spec/year_spec.rb
@@ -46,4 +46,50 @@ RSpec.describe Year do
 
   end
 
+  context "#header" do
+    it "matches cal for 1901" do
+      month = Year.new(1901)
+      month.send(:header).should == "                             1901\n\n"
+    end
+    it "matches cal for 2014" do
+      month = Year.new(2014)
+      month.send(:header).should == "                             2014\n\n"
+    end
+    it "matches cal for 3000" do
+      month = Year.new(3000)
+      month.send(:header).should == "                             3000\n\n"
+    end
+  end
+
+  context "#build_january_february_march_title_line" do
+    it "matches January February March with spaces" do
+      year = Year.new(1900)
+      actual = "      January               February               March\n"
+      year.send(:build_january_february_march_title_line).should == actual
+    end
+  end
+
+
+  context "#build_days_array" do
+    it "matches the accumulation of new month instances" do
+      january = Month.new(1, 2000).days
+      february = Month.new(2, 2000).days
+      march = Month.new(3, 2000).days
+      april = Month.new(4, 2000).days
+      may = Month.new(5, 2000).days
+      june = Month.new(6, 2000).days
+      july = Month.new(7, 2000).days
+      august = Month.new(8, 2000).days
+      september = Month.new(9, 2000).days
+      october = Month.new(10, 2000).days
+      november = Month.new(11, 2000).days
+      december = Month.new(12, 2000).days
+
+      year = Year.new(2000).days
+
+      actual = [january, february, march, april, may, june, july, august, september, october, november, december]
+      year.should == actual
+    end
+  end
+
 end
