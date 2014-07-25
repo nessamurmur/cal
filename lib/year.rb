d3b3dd1fca6a7ea883721cb2cadb234e5630fd7f
Jack Newton <eternal.turtles@gmail.com>
2014-07-25 03:16:14 -0500

    building first quarter

--- lib/year.rb
+++ lib/year.rb
@@ -1,7 +1,9 @@
 class Year
 
-  # CELL = "  "
+  CELL = "  "
+  CELL_GUTTER = " "
   MONTH_GUTTER = "  "
+  MONTHS = [nil, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
   attr_reader :days
 
@@ -32,34 +34,68 @@ class Year
     @quarter = ""
     @quarter << build_months_title_line("January", "February", "March")
     @quarter << build_weeknames_line
-    @quarter << build_first_line
+    # build_first_line
+    build_line(0..6)
+    build_line(7..13)
+    build_line(14..20)
+    build_line(21..27)
+    build_line(28..34)
+    build_line(35..41)
+    # build_line
+    # @quarter << @line
   end
 
-  def build_first_line
-    build_first_line_array
+  def build_line(range)
+    push_nil_to_end_of_months
+    build_line_array(range)
+    strip_month_day_indexes
     convert_line_to_string
+    @quarter << @line
   end
 
-  def build_first_line_array
-    range = 0..6
+  def push_nil_to_end_of_months
+    @days[0].fill([nil, nil], (@days[0].length)..41)
+    @days[1].fill([nil, nil], (@days[1].length)..41)
+    @days[2].fill([nil, nil], (@days[2].length)..41)
+  end
+
+  def build_line_array(range)
+    # range = 0..6
     @line_array = []
     @line_array << @days[0][range]
     @line_array << @days[1][range]
     @line_array << @days[2][range]
   end
 
-  def convert_line_to_string
+  def strip_month_day_indexes
     temp = []
     @line_array.flatten!(1)
     @line_array.collect do |i|
       temp << i[0]
     end
     @line_array = temp
-    @line_array.inspect
+  end
+
+  def convert_line_to_string
+    @line = ""
+    @line_array.each_with_index do |i, index|
+      if i.nil?
+        @line << CELL.dup
+      elsif i.class == Fixnum
+        @line << "#{i}".rjust(2)
+      end
+      if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 7 || index == 8 || index == 9 || index == 10 || index == 11 || index == 12 || index == 14 || index == 15 || index == 16 || index == 17 || index == 18 || index == 19
+        @line << CELL_GUTTER.dup
+      elsif index == 6 || index == 13
+        @line << MONTH_GUTTER.dup
+      elsif index == 20
+        @line << "\n"
+      end
+    end
   end
 
   def build_months_title_line(month_1, month_2, month_3)
-    gutter = "  "
+    gutter = MONTH_GUTTER.dup
     title_line = ""
     title_line << month_1.center(20)
     title_line << gutter
